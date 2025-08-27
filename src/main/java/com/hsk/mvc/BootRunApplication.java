d374a4b6d367d9460badaf0812eaa247262fa37e
HSK-DunDunRST <gon5073@naver.com>
2025-08-28 01:19:40 +0900

    Created Project

---
+++ src/main/java/com/hsk/mvc/BootRunApplication.java
@@ -0,0 +1,13 @@
+package com.hsk.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsk/mvc/BootRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsk.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
