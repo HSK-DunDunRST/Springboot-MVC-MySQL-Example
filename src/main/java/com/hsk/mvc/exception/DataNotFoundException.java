6fa43f567377ce9991885a13fce74a62376ae87b
HSK-DunDunRST <gon5073@naver.com>
2025-08-28 01:22:34 +0900

    Add Exception

---
+++ src/main/java/com/hsk/mvc/exception/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package com.hsk.mvc.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Data not found")
+public class DataNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
